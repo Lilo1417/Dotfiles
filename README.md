4b290fa5b8f405255e5504743677aad22e1ea6b9
lilo1417 <linoallg@schafferts.ch>
2026-03-11 22:37:16 +0100

    Changed Readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Lilo1417 Hyprland Setup
+# Lilos Hyprland Setup
 ## Screenshots
 ### A sample Background with bar written in eww on Top
 ![DragonBackground](assets/Images/DragonBackground.png)
@@ -6,6 +6,6 @@
 ### Another Background with the default Terminal
 ![BlackHoleWithTerminal](assets/Images/BlackHoleTerminal.png)
 
-### Another Background with the default rofi config
+### Another Background with the rofi running in drun mode
 ![Expedition33Withrofi](assets/Images/Expedition33Rofi.png)
 
---
+++ wal/templates/colors.fish
@@ -0,0 +1,19 @@
+set -U fish_color_normal        {foreground.strip}
+set -U fish_color_command       {color2.strip}
+set -U fish_color_param         {color4.strip}
+set -U fish_color_redirection   {color1.strip}
+set -U fish_color_comment       {color8.strip}
+set -U fish_color_error         {color9.strip}
+set -U fish_color_escape        {color5.strip}
+set -U fish_color_operator      {color5.strip}
+set -U fish_color_end           {color12.strip}
+set -U fish_color_quote         {color6.strip}
+set -U fish_color_autosuggestion brblack
+set -U fish_color_user          brgreen
+set -U fish_color_host          {foreground.strip}
+set -U fish_color_cwd           {color2.strip}
+set -U fish_color_search_match  bryellow --background=brblack
+set -U fish_pager_color_prefix  white --bold --underline
+set -U fish_pager_color_completion {color4.strip}
+set -U fish_pager_color_description {color6.strip}
+
